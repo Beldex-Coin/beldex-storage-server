253faf4be513af156f86de3e8dab8bf1a876fb0e
Tore-tto <toretto@beldex.io>
2025-10-27 11:16:26 +0530

    version bump to v2.4.0

--- beldexss/mnode/master_node.h
+++ beldexss/mnode/master_node.h
@@ -56,11 +56,11 @@ using hf_revision = std::pair<int, int>;
 inline constexpr hf_revision STORAGE_SERVER_HARDFORK = {12, 1};
 
 // The hardfork at which we start testing QUIC reachability
-inline constexpr hf_revision QUIC_REACHABILITY_TESTING = {20, 0};
+inline constexpr hf_revision QUIC_REACHABILITY_TESTING = {21, 0};
 
 // The storage server version at which initial handshaking is supported before attempting a swarm
 // message transfer.
-inline constexpr std::array<uint16_t, 3> NEW_SWARM_MEMBER_HANDSHAKE_VERSION = {2, 4, 0}; //2.10.0(SS-version)
+inline constexpr std::array<uint16_t, 3> NEW_SWARM_MEMBER_HANDSHAKE_VERSION = {2, 4, 0}; 
 
 // The hardfork at which we start allowing 30d TTLs in private namespaces.
 inline constexpr hf_revision HARDFORK_EXTENDED_PRIVATE_TTL = {12, 3};
